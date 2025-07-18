727c9e2c1effba9a8e74e4179ab4fccf56e82625
Brendan Dileo <dileob23@gmail.com>
2025-07-18 15:36:46 -0400

    Add addNode method

--- src/main/java/org/example/Graph.java
+++ src/main/java/org/example/Graph.java
@@ -10,9 +10,21 @@ public class Graph<T> {
     // Adjacency list to store the graph
     private Map<T, List<T>> adjacencyList;
 
-    // Conbstructs a graph with an empty adjacency list
+    // Constructs a graph with an empty adjacency list
     public Graph() {
         this.adjacencyList = new HashMap<>();
     }
+
+    /**
+     * Adds a node to the graph
+     * 
+     * @param node The node of generic type to be added to the graph
+     */
+    public void addNode(T node) {
+        // Add a new node to the graph if it doesn't already exist
+        adjacencyList.putIfAbsent(node, new ArrayList<>());
+    }
+
+    
 
 }
